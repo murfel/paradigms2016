f4d850949341aed90183409d8c0dce56aad65fd5
Natasha Murashkina <murnatty@gmail.com>
2016-09-24 17:41:37 +0300

    Misc fixes

--- para-task-02/find_duplicates.py
+++ para-task-02/find_duplicates.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 
-from os import path, sys, walk
-from hashlib import sha1
+import collections
+import hashlib
+import os
+import sys
 
 
-def find_duplicate(directory):
-    file_group = dict()
+def find_duplicates(directory):
+    file_group = collections.defaultdict(set)
 
-    offset = len(directory)
-    for dirpath, dirname, files in walk(directory):
+    for dirpath, _, files in os.walk(directory):
         for filename in files:
-            rel_path = path.join(dirpath, filename)
+            rel_path = os.path.join(dirpath, filename)
             if (filename.startswith('.') or filename.startswith('~')
-                    or path.islink(rel_path)):
+                    or os.path.islink(rel_path)):
                 continue
-            file_hash = sha1()
+            file_hash = hashlib.sha1()
             with open(rel_path, mode='rb') as content:
                 while True:
                     data = content.read(1024)
                     if not data:
                         break
                     file_hash.update(data)
-            file_hash = file_hash.hexdigest()
-            if file_hash not in file_group:
-                file_group[file_hash] = set()
-            file_group[file_hash].add(rel_path[offset:])
+            file_hexdigest = file_hash.hexdigest()
+            file_shortname = os.path.relpath(rel_path, directory)
+            file_group[file_hexdigest].add(file_shortname)
 
     for v in file_group.values():
         if len(v) != 1:
@@ -33,11 +33,11 @@ def find_duplicate(directory):
 
 def main():
     if len(sys.argv) != 2:
-        print('Usage: ./find_duplicate.py path/to/directory/')
+        print('Usage: ./find_duplicates.py path/to/directory')
         sys.exit(1)
 
     directory = sys.argv[1]
-    find_duplicate(directory)
+    find_duplicates(directory)
 
 if __name__ == '__main__':
     main()
