7214c302bff235d269b280838710baa9e18813de
Natasha Murashkina <murnatty@gmail.com>
2016-09-28 19:33:40 +0300

    Misc functional and cosmetic changes Shorten strassen, add bin_ceil, rename split_np_array into split_mx_into_four, remove input_np_array (i/o is now done with np methods), add docstrings. Phew, how much work for one commit!

--- para-task-03/strassen.py
+++ para-task-03/strassen.py
@@ -4,52 +4,34 @@ import sys
 import numpy as np
 
 
-def input_np_array(n):
-    # n rounded up to the nearest power of 2
-    n_rounded = 1 << (n - 1).bit_length()
-    a = np.zeros((n_rounded, n_rounded), dtype=int)
-    a[:n, :n] = np.array([[int(i) for i in input().split()] for _ in range(n)])
-    return a
+def bin_ceil(n):
+    """Round n up to the nearest power of 2."""
+    return 1 << (n - 1).bit_length()
 
 
-def split_np_array(a):
-    m = len(a) / 2
-    return a[:m, :m], a[:m, m:], a[m:, :m], a[m:, m:]
+def split_mx_into_four(a):
+    """Split a 2D numpy array into four equal arrays."""
+    return [mx for submx in np.split(a, 2) for mx in np.hsplit(submx, 2)]
 
 
 def strassen(a, b):
-    n = len(a)
-    if n == 1:
+    """Multiply square matrices a and b using the Strassen algorithm."""
+    if len(a) == 1:
         return np.dot(a, b)
-    c = np.ones((n, n), dtype=int)
-    a11, a12, a21, a22 = split_np_array(a)
-    b11, b12, b21, b22 = split_np_array(b)
-    c11, c12, c21, c22 = split_np_array(c)
 
-    p = []
-    for i in range(7):
-        p.append(np.empty((n, n), dtype=int))
+    a11, a12, a21, a22 = split_mx_into_four(a)
+    b11, b12, b21, b22 = split_mx_into_four(b)
 
-    p[0] = strassen(a11 + a22, b11 + b22)
-    p[1] = strassen(a21 + a22, b11)
-    p[2] = strassen(a11, b12 - b22)
-    p[3] = strassen(a22, b21 - b11)
-    p[4] = strassen(a11 + a12, b22)
-    p[5] = strassen(a21 - a11, b11 + b12)
-    p[6] = strassen(a12 - a22, b21 + b22)
+    p1 = strassen(a11 + a22, b11 + b22)
+    p2 = strassen(a21 + a22, b11)
+    p3 = strassen(a11, b12 - b22)
+    p4 = strassen(a22, b21 - b11)
+    p5 = strassen(a11 + a12, b22)
+    p6 = strassen(a21 - a11, b11 + b12)
+    p7 = strassen(a12 - a22, b21 + b22)
 
-    c11 = p[0] + p[3] - p[4] + p[6]
-    c12 = p[2] + p[4]
-    c21 = p[1] + p[3]
-    c22 = p[0] - p[1] + p[2] + p[5]
-
-    m = n / 2
-    c[:m, :m] = c11
-    c[:m, m:] = c12
-    c[m:, :m] = c21
-    c[m:, m:] = c22
-
-    return c
+    return np.hstack((np.vstack((p1 + p4 - p5 + p7, p3 + p5)),
+                      np.vstack((p2 + p4, p1 - p2 + p3 + p6))))
 
 
 def main():
@@ -58,16 +40,16 @@ def main():
         sys.exit(1)
 
     n = int(input())
-    n_rounded = 1 << (n - 1).bit_length()
-    a = input_np_array(n)
-    b = input_np_array(n)
+    n_rounded = bin_ceil(n)
+
+    a = np.zeros((n_rounded, n_rounded), dtype=int)
+    b = np.zeros((n_rounded, n_rounded), dtype=int)
+    a[:n, :n], b[:n, :n] = np.vsplit(np.loadtxt(sys.stdin), 2)
 
     c = strassen(a, b)
 
-    for row in c[:n, :n]:
-        for cell in row:
-            print(cell, end=' ')
-        print()
+    np.savetxt(sys.stdout.buffer, c[:n, :n], fmt='%d')
+
 
 if __name__ == '__main__':
     main()
