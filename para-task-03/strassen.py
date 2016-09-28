029364d6f8cba32a60934a073e866aee666495f1
Natasha Murashkina <murnatty@gmail.com>
2016-09-29 00:01:56 +0300

    Fix mult bug (unnecessary diagonal swap)

--- para-task-03/strassen.py
+++ para-task-03/strassen.py
@@ -30,8 +30,8 @@ def strassen(a, b):
     p6 = strassen(a21 - a11, b11 + b12)
     p7 = strassen(a12 - a22, b21 + b22)
 
-    return np.hstack((np.vstack((p1 + p4 - p5 + p7, p3 + p5)),
-                      np.vstack((p2 + p4, p1 - p2 + p3 + p6))))
+    return np.vstack((np.hstack((p1 + p4 - p5 + p7, p3 + p5)),
+                      np.hstack((p2 + p4, p1 - p2 + p3 + p6))))
 
 
 def main():
