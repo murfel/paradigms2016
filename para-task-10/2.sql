5fb124a738356f75d00cd4f97b322e07920d919d
Natasha Murashkina <murnatty@gmail.com>
2016-12-07 22:34:35 +0300

    10: 2

--- para-task-10/2.sql
+++ para-task-10/2.sql
@@ -1,4 +1,5 @@
-select country.name, max(literacyrate.rate)
-from (literacyrate join country on country.code = literacyrate.countrycode)
-group by country.name having literacyrate.year = max(literacyrate.year)
-order by literacyrate.rate desc limit 1;
+select name, max(rate) from
+(select country.name, literacyrate.rate
+from country join literacyrate on country.code = literacyrate.countrycode
+group by country.code
+having literacyrate.year = max(literacyrate.year));
